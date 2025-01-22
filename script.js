82c406197b9a33c1ee0a743e5a96481842c3170d
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-22 18:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.querySelector('input[type="button"]').addEventListener('click', function () {
+    // Get the dropdown list
+    const colorSelect = document.getElementById('colorSelect');
+    // Get the selected option
+    const selectedOption = colorSelect.selectedIndex;
+    if (selectedOption !== -1) {
+        colorSelect.options[selectedOption].remove();
+    }
+});
